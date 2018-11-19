7136c33e1acdabad9e54ecc9bdd53174470511b7
arun <arun.rajan@25northsoft.com>
2018-11-19 14:51:29 +0530

    code clearning

--- src/App.js
+++ src/App.js
@@ -2,68 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import  Header  from './components/header';
 import  InputText  from './components/inputText';
-import  SubmitButton  from './components/button';
 import { Button } from "carbon-components-react";
-
-import { TextInput } from "carbon-components-react";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {name: ''};
+    this.state = {email: ''};
+    this.state = {phone: ''};
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-
-
-  handleChange(e) {
-    
-    console.log("text changed");
-  }
-//   handleChange(e){
-//     this.setState({[e.target.id]: e.target.value});
-//  }
   handleSubmit(event) {
-    //console.log(event);
-      alert('A name was submitted: ' + this.state.value);
+      alert('A name was submitted: ' + this.state.name);
+      alert('An email was submitted: ' + this.state.email);
+      alert('A phone was submitted: ' + this.state.phone);
       event.preventDefault();
   }
-  updateInput(event){
-    console.log("changed text");
-    //this.setState({username : event.target.value})
-    }
-  updateInputValue(evt) {
-    console.log("changed value");
+  onChangeName = (val) => {
     this.setState({
-      value: evt.target.value
-    });
-  }
-  onUpdate = (val) => {
+      name: val
+    })
+  };
+  onChangeEmail = (val) => {
     this.setState({
-      val: val
+      email: val
+    })
+  };
+  onChangePhone = (val) => {
+    this.setState({
+      phone: val
     })
   };
   
   render() {
     return (
-  <form  onSubmit={this.handleSubmit} className="padding-layout">
-  <Header header_title="Dashboard"/>
-  <section className="bx--col-xs-12 about-app">React with IBM Carbon UI</section>
-					<div className="bx--col-md-4 bx--col-xs-12 label-div">
-            <TextInput  id="test2" className="bx--text-input" type="text" value={this.state.value} labelText="First Name" onChange={this.updateInputValue.bind(this)} placeholder="Please enter first name"/>  
-            {/* <input type="text" onChange={this.updateInputValue.bind(this)}/> */}
-          </div>
-          <div className="bx--col-md-4 bx--col-xs-12 label-div">
-            <InputText onUpdate={this.onUpdate} input_id="test2" input_type="text" error_text="A valid value is required" label_text="Email id" placeholder_text="Please enter email id"/>
-					</div>
-          <div className="bx--col-md-4 bx--col-xs-12 label-div">
-            <InputText input_id="test2" label_text="Phone number" placeholder="Please enter phone"/>
-					</div>
-          <div className="bx--col-md-4 bx--col-xs-12 label-div">
-            <Button type="submit" button_name="Submit" button_type="primary" onClick={this.clickme}>submit</Button>
-          </div>
-  </form>
+    <form  onSubmit={this.handleSubmit} className="padding-layout">
+    <Header header_title="Dashboard"/>
+    <section className="bx--col-xs-12 about-app">React with IBM Carbon UI</section>
+            <div className="bx--col-md-4 bx--col-xs-12 label-div">
+              <InputText onUpdate={this.onChangeName} input_id="test2" helper_text="Name as on passport" input_type="text" error_text="A valid value is required" label_text="First name" placeholder_text="Please enter name"/>
+            </div>
+            <div className="bx--col-md-4 bx--col-xs-12 label-div">
+              <InputText onUpdate={this.onChangeEmail} input_id="test2" helper_text="Official email id" input_type="text" error_text="A valid value is required" label_text="Email id" placeholder_text="Please enter email id"/>
+            </div>
+            <div className="bx--col-md-4 bx--col-xs-12 label-div">
+              <InputText onUpdate={this.onChangePhone} input_id="test3" helper_text="Official Phone" input_type="text" error_text="A valid value is required" label_text="Phone number" placeholder_text="Please enter phone"/>
+            </div>
+            <div className="bx--col-md-4 bx--col-xs-12 label-div">
+              <Button type="submit" button_name="Submit" button_type="primary" onClick={this.clickme}>submit</Button>
+            </div>
+    </form>
     );
   }
 }
--- src/components/inputText.js
+++ src/components/inputText.js
@@ -1,30 +1,8 @@
-// import React from 'react';
-// import { TextInput } from "carbon-components-react";
-
-// const InputText = (props) => (
-//     <TextInput
-//         className="bx--text-input"
-//         id="test2"
-//         labelText= {props.label_text}
-//         type=""
-//         placeholder= {props.placeholder_text}
-//         hideLabel={false}
-//         invalidText="A valid value is required"
-//         name={props.label_text}
-//         // helperText="Optional helper text."
-//     />
-//   )
-
-//   export default InputText;
-
-
-
 import React from 'react';
 import { TextInput } from "carbon-components-react";
 class InputText extends React.Component {
     constructor(props) {
         super(props);
-    
         this.state = {
           val: ""
         }
@@ -35,9 +13,7 @@ update = (e) => {
     this.setState({val: e.target.value});
   };
   render() {
-    return (
- //InputText = (props) => (
-     
+    return (    
     <TextInput
         className={this.props.class}
         id={this.props.input_id}
@@ -48,11 +24,10 @@ update = (e) => {
         invalidText= {this.props.error_text}
         name={this.props.label_text}
         onChange={this.update}
-        // helperText="Optional helper text."
+        helperText={this.props.helper_text}
     />
- // )
   );
 }
 
 }
-  export default InputText;
+export default InputText;
